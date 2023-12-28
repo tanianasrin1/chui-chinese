af838a6c204d4cd9aab344b6d806bf222a0c7734
tanianasrin1 <tasrineva169@gmail.com>
2023-12-28 17:29:35 +0600

    layout updated

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,16 @@
 import Layout from "@/components/Layout/Layout";
+import MetaComponent from "@/components/MetaComponent/MetaComponent";
 import Project from "@/components/Project/Project";
+import Head from "next/head";
 import React from "react";
 
 const index = () => {
   return (
     <>
+      <Head>
+      <title>Project | </title>
+      </Head>
+     
       <Layout>
         <Project />
       </Layout>
