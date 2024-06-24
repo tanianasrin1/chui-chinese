c2f6ef329216d475e247fadaa358c629da065c4c
tanianasrin1 <tasrineva169@gmail.com>
2024-06-24 09:35:14 +0600

    google tag manager added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,38 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+    <Head>
+        <meta name="title" content="Boiler plate" />
+        <meta
+          name="description"
+          content="Boiler plate"
+        />
+        <meta name="keywords" content="Lesson Thornton Drive" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MH5GSV4K');
+            `,
+          }}
+        />
+      </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MH5GSV4K"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
