a291608c65d3a6af8d54be22f4ceff28cc657e09
tanianasrin1 <tasrineva169@gmail.com>
2024-11-28 20:53:09 +0600

    content updated

--- src/components/Offer/WeOffer.js
+++ src/components/Offer/WeOffer.js
@@ -6,7 +6,7 @@ const WeOffer = () => {
     <div className="lg:py-16 py-10">
       <div className="container-sk">
         <h6 className="text-black font-black text-4xl md:text-[48px] md:leading-[80px] text-center">
-         We Offer
+         Our Offer
         </h6>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4 lg:mt-6">
               <OfferCard/>
